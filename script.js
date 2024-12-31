b60d0e028b80ca41c1b2d0e02db72f216fa1cdf3
Omar <o.alhammadi.93@gmail.com>
2024-12-31 11:46:08 +0400

    a button with style has been added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window.onload = function() {
     alert("Welcome to interactive website");
 };
+
+document.getElementById("clickme").onclick = function() {
+    alert("This button has been clicked");
+};
